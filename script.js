f551b1d48ce85c949282bf831108bdd2f1e9366f
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-07-21 15:33:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-window.promises = [];
+window.promises = [one(), two(), three() , four() , five()];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+let one=new promise((resolve,reject)=>{
+	setTimeout(resolve, 1000);
+            }).then(promise.any() {
+                document.getElementById("output").innerHTML = 
+                "first promise";
+            });
+})
