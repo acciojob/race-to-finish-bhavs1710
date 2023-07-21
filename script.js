d8fe1cb9964fefd89fc44d636f62910e285bf8c0
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-07-21 15:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-window.promises = [one(), two(), three() , four() , five()];
+window.promises = 
 
 // Do not change the code above this
-// add your promises to the array `promises`
-let one=new promise((resolve,reject)=>{
-	setTimeout(resolve, 1000);
-            }).then(promise.any() {
-                document.getElementById("output").innerHTML = 
-                "first promise";
-            });
-})
+// add your promises to the array `promises
+  // Function to generate a random time between min and max (inclusive)
+  function getRandomTime(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  // Function to create a promise that resolves after a random time
+  function createRandomPromise() {
+    const randomTime = getRandomTime(1000, 5000); // Random time between 1 and 5 seconds
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(randomTime);
+      }, randomTime);
+    });
+  }
+
+  // Create an array of five promises
+  const promises = Array.from({ length: 5 }, createRandomPromise);
+
+  // Use Promise.any to wait for the first promise to resolve
+  Promise.any(promises)
+    .then((result) => {
+      // Print the result to the div with id "output"
+      const outputDiv = document.getElementById("output");
+      outputDiv.textContent = `Resolved after ${result} milliseconds.`;
+    })
+    .catch((error) => {
+      console.error("Error occurred:", error);
+    });
+
+
